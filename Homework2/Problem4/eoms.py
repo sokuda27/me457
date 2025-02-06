cd30dc62643feafd580e8864f39307233b09fd2f
Taaseen <taaseen.jahan@gmail.com>
2025-02-06 16:42:55 -0500

    added files

---
+++ Homework2/Problem4/eoms.py
@@ -0,0 +1,14 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Inputs
+F = np.array([])
+J_b = np.array([])
+
+# Parameters
+m = np.array([])
+J = np.array([])
+
+# Initial Conditions
+
