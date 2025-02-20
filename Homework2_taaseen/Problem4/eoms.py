985a34be993e49f9ba82a7303151d039770e12e2
Taaseen <taaseen.jahan@gmail.com>
2025-02-19 21:58:11 -0500

    Updated Taaseen's eoms.py

--- Homework2_taaseen/Problem4/eoms.py
+++ Homework2_taaseen/Problem4/eoms.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+import integrators as intg
 
 ######################################################################
 # Author: Taaseen Jahan                                              #
@@ -45,65 +46,73 @@ J = np.array([[J_x, -J_xy, -J_xz],  # Inertia Matrix
 
 # Initial Conditions
 ## Translational Kinematics State
-p_n = 1
-p_e = 1
-p_d = 1
-pos = np.array([p_n, p_e, p_d]) # Position Vector
+p_n_0 = 1
+p_e_0 = 1
+p_d_0 = 1
+pos_0 = np.array([p_n_0, p_e_0, p_d_0]) # Position Vector
 ## Rotational Kinematics State
-psi = np.deg2rad(1)
-theta = np.deg2rad(1)
-phi = np.deg2rad(1)
-euler = np.array([psi, theta, phi]) # Euler Angles
+psi_0 = np.deg2rad(1)
+theta_0 = np.deg2rad(1)
+phi_0 = np.deg2rad(1)
+euler_0 = np.array([psi_0, theta_0, phi_0]) # Euler Angles
 ## Translational Dynamics State
-u = 1
-v = 1
-w = 1
-V = np.array([u, v, w]) # Linear Velocity Vector
+u_0 = 1
+v_0 = 1
+w_0 = 1
+V_0 = np.array([u_0, v_0, w_0]) # Linear Velocity Vector
 ## Rotational Dynamics State
-p = 1
-q = 0
-r = 2
-w = np.array([p, q, r]) # Angular Velocity Vector
+p_0 = 1
+q_0 = 0
+r_0 = 2
+W_0 = np.array([p_0, q_0, r_0]) # Angular Velocity Vector
 
-w_bb = np.array([[0, -r, q],
-                 [r, 0, -p],
-                 [-q, p, 0]])
+IC = np.array([p_n_0, p_e_0, p_d_0, psi_0, theta_0, phi_0, u_0, v_0, w_0, p_0, q_0, r_0]) # Initial Conditions Vector
 
-# Solve EOMs
-pos_rot_matrix = np.array([[np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
-                         [np.cos(theta)*np.sin(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) + np.cos(phi)*np.cos(psi), np.cos(phi)*np.sin(theta)*np.sin(psi) + np.sin(phi)*np.cos(psi)],
-                         [-np.sin(theta), np.sin(phi)*np.cos(theta), np.cos(phi)*np.cos(theta)]])
-# pos_solution = np.dot(pos_rot_matrix, pos)
-# V_solution = (1/M**2)*np.dot(-w_bb, V) + f_b
-euler_rot_matrix = np.array([[1, np.sin(phi)*np.tan(theta), np.cos(phi)*np.tan(theta)],
-                            [0, np.cos(phi), -np.sin(phi)],
-                            [0, np.sin(phi)/np.cos(theta), np.cos(phi)/np.cos(theta)]])
-# euler_solution = np.dot(euler_rot_matrix, euler)
-# w_solution = np.dot(np.linalg.inv(J), (m_b-np.dot(w_bb,np.dot(J,w))))
+def f(t, x, u_i):
+    p_n, p_e, p_d, psi, theta, phi, u, v, w, p, q, r = x
+    
+    # Rotation Matrices
+    pos_rot_matrix = np.array([[np.cos(theta)*np.cos(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.cos(phi)*np.sin(theta)*np.cos(psi) + np.sin(phi)*np.sin(psi)],
+                             [np.cos(theta)*np.sin(psi), np.sin(phi)*np.sin(theta)*np.cos(psi) + np.cos(phi)*np.cos(psi), np.cos(phi)*np.sin(theta)*np.sin(psi) + np.sin(phi)*np.cos(psi)],
+                             [-np.sin(theta), np.sin(phi)*np.cos(theta), np.cos(phi)*np.cos(theta)]])
+    euler_rot_matrix = np.array([[1, np.sin(phi)*np.tan(theta), np.cos(phi)*np.tan(theta)],
+                                [0, np.cos(phi), -np.sin(phi)],
+                                [0, np.sin(phi)/np.cos(theta), np.cos(phi)/np.cos(theta)]])
+    
+    # Gamma Values
+    Gamma = J_x*J_z-J_xz**2
+    Gamma1 = J_xz*(J_x-J_y+J_z)/Gamma
+    Gamma2 = (J_z*(J_z-J_y)+J_xz**2)/Gamma
+    Gamma3 = J_z/Gamma
+    Gamma4 = J_xz/Gamma
+    Gamma5 = (J_z-J_x)/J_y
+    Gamma6 = J_xz/J_y
+    Gamma7 = ((J_x-J_y)*J_x+J_xz**2)/Gamma
+    Gamma8 = J_x/Gamma
+    
+    pos_dot = np.dot(pos_rot_matrix, np.array([u, v, w])) # From Slide 15
+    V_dot = np.array([r*v-q*w, p*w-r*u, q*u-p*v]) + (1/M)*f_b # From Slide 15
+    euler_dot = np.dot(euler_rot_matrix, np.array([p, q, r])) # From Slide 15
+    W_dot = np.array([Gamma1*p*q-Gamma2*q*r, Gamma5*p*r-Gamma6*(p**2-r**2), Gamma7*p*q-Gamma1*q*r]) + np.array([Gamma3*l+Gamma4*n, (1/J_y)*M, Gamma4*l+Gamma8*n]) # From Slide 15
+    
+    return np.concatenate((pos_dot, V_dot, euler_dot, W_dot))
+# Time Stepping Parameters
+dt = 0.1; n = 10
 
-pos_dot = pos_rot_matrix*pos
-V_dot = (1/M)*(np.cross(-w,V) + f_b)
-euler_dot = euler_rot_matrix*euler
-w_dot= np.linalg.inv(J)*(M-np.cross(w, np.cross(J,w)))
+t = 0; x = IC; u_i = 0
+integrator = intg.RK4(dt, f)
+t_history = [0]
+x_history = [x]
+for i in range(n):
+    
+    x = integrator.step(t, x, u_i)
+    t = (i+1) * dt
 
-dots = np.concatenate(pos_dot, V_dot, euler_dot, w_dot)
-params = np.concatenate(pos, V, euler, w)
+    t_history.append(t)
+    x_history.append(x)
 
-def model(y, t, params):
-    """
-    Defines the system of 12 ODEs.
-
-    Args:
-        y (array): A state vector with 12 elements.
-        t (float): Time.
-        params (tuple): A tuple of parameters for the model.
-
-    Returns:
-        dydt (array): An array of derivatives for each state variable.
-    """
-    # Example: Linear system with interaction
-    A = params
-    dydt = np.dot(A, y)
-    return dydt
-
-solve = odeint(model, y0, t, args=(dots,))
+intg.__doc__
+plt.figure()
+plt.title('Graph')
+plt.plot(t_history, x_history)
+plt.show()
