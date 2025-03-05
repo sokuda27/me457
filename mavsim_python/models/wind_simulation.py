a4cbd1edfb47c55225c7e222198175d833e75f75
Saira Billah <saira.billah@cooper.edu>
2025-03-05 02:06:42 -0500

    updating ch4 check

--- mavsim_python/models/wind_simulation.py
+++ mavsim_python/models/wind_simulation.py
@@ -17,7 +17,7 @@ class WindSimulation:
         L_v = L_u
         L_w = 50
         V_a = 25
-        
+        gust_flag = True
         if gust_flag == True:
             sigma_u = 1.06
             sigma_v = sigma_u
@@ -36,5 +36,6 @@ class WindSimulation:
         gust = np.array([[self.u_w.update(np.random.randn())],
                          [self.v_w.update(np.random.randn())],
                          [self.w_w.update(np.random.randn())]])
+        gust = np.array([[0.],[0.],[0.]])
         return np.concatenate(( self._steady_state, gust ))
 
