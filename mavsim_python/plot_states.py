8df755b8e8b1c913cd442ea201603d6286ce5f03
sokuda27 <sonampmokuda@gmail.com>
2025-03-06 00:04:57 -0500

    a graph?

--- mavsim_python/plot_states.py
+++ mavsim_python/plot_states.py
@@ -1,5 +1,7 @@
 import numpy as np
-from models.mav_dynamics import MavDynamics
+import matplotlib.pyplot as plt
+
+from models.mav_dynamics_control import MavDynamics
 from models.wind_simulation import WindSimulation
 import parameters.simulation_parameters as SIM
 from models.trim import compute_trim
@@ -16,24 +18,42 @@ delta = trim_input  # set input to constant constant trim input
 sim_time = SIM.start_time
 end_time = 60 
 
+# with trim
+delta_e_trim = delta.elevator
+delta_a_trim = delta.aileron
+delta_r_trim = delta.rudder
+
+# no input
+delta.elevator = 0
+delta.aileron = 0
+delta.rudder = 0
+
+pnorth_hist = [mav._state[0]]
+peast_hist = [mav._state[1]]
+pdown_hist = [mav._state[2]]
+time = [0]
+
 while sim_time < end_time:
-    current_wind = wind.update() 
+    current_wind = wind.update()
+    mav.update(delta, current_wind)
 
-# intg.__doc__
+    time.append(sim_time)
+
+    pnorth_hist.append(mav._state.item(0))
+    peast_hist.append(mav._state.item(1))
+    pdown_hist.append(mav._state.item(2))
+
+    sim_time += SIM.ts_simulation
+
 # plt.figure()
-# plt.title('pn')
-# p_north = [arr[0] for arr in x_history]
-# p_east = [arr[1] for arr in x_history]
-# p_down = [arr[2] for arr in x_history]
-# plt.plot(t_history, p_north)
-# #plt.legend(["pn", "pe", "pd"])
+# plt.plot(time, pnorth_hist)
 # plt.show()
 
-# plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.plot3D(p_north, p_east, p_down)
-# ax.set_title('Path of aircraft')
-# ax.set_xlabel('north', fontsize=10)
-# ax.set_ylabel('east', fontsize=10)
-# ax.set_zlabel('down', fontsize=10)
-# plt.show()
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(pnorth_hist, peast_hist, pdown_hist)
+ax.set_title('Path of aircraft')
+ax.set_xlabel('north', fontsize=10)
+ax.set_ylabel('east', fontsize=10)
+ax.set_zlabel('down', fontsize=10)
+plt.show()
