ab72ad90d19e7a9142c41dc2ca76a592f5d9ada9
sokuda27 <sonampmokuda@gmail.com>
2025-03-20 15:36:41 -0400

    cant spell

--- mavsim_python/controllers/autopilot.py
+++ mavsim_python/controllers/autopilot.py
@@ -60,7 +60,7 @@ class Autopilot:
 	
 	#### TODO #####
         # lateral autopilot
-        chi_c = wrap(cmd.course_commnd, state.chi)
+        chi_c = wrap(cmd.course_command, state.chi)
         phi_c = self.saturate(cmd.phi_feedforward + self.course_from_roll.update(chi_c, state.chi) - np.radians(30), np.radians(30))
         delta_a = self.roll_from_aileron.update(phi_c, state.phi, state.p)
         delta_r = self.yaw_damper.update(state.r)
