c0d6728f5f58ecbcb62181c0286ed9dba324b030
sokuda27 <sonampmokuda@gmail.com>
2025-03-20 15:27:42 -0400

    lat and long to autopilot.py

--- mavsim_python/controllers/autopilot.py
+++ mavsim_python/controllers/autopilot.py
@@ -60,16 +60,23 @@ class Autopilot:
 	
 	#### TODO #####
         # lateral autopilot
-
+        chi_c = wrap(cmd.course_commnd, state.chi)
+        phi_c = self.saturate(cmd.phi_feedforward + self.course_from_roll.update(chi_c, state.chi) - np.radians(30), np.radians(30))
+        delta_a = self.roll_from_aileron.update(phi_c, state.phi, state.p)
+        delta_r = self.yaw_damper.update(state.r)
 
         # longitudinal autopilot
-
+        h_c = self.saturate(cmd.altitude_command, state.h - AP.altitude_zone, state.h + AP.altitude_zone)
+        theta_c = self.altitude_from_pitch.update(h_c, state.h)
+        delta_e = self.pitch_from_elevator.update(h_c, state.h)
+        delta_t = self.airspeed_from_throttle.update(cmd.airspeed_command, state.Va)
+        delta_t = self.yaw_damper.update(state.r)
 
         # construct control outputs and commanded states
-        delta = MsgDelta(elevator=0,
-                         aileron=0,
-                         rudder=0,
-                         throttle=0)
+        delta = MsgDelta(elevator=delta_e,
+                         aileron=delta_a,
+                         rudder=delta_r,
+                         throttle=delta_t)
         self.commanded_state.altitude = 0
         self.commanded_state.Va = 0
         self.commanded_state.phi = 0
