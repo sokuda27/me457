5247448d8243a466553d852ef6b14b4229472ffd
sokuda27 <sonampmokuda@gmail.com>
2025-03-20 15:49:58 -0400

    command message update

--- mavsim_python/controllers/autopilot.py
+++ mavsim_python/controllers/autopilot.py
@@ -77,11 +77,11 @@ class Autopilot:
                          aileron=delta_a,
                          rudder=delta_r,
                          throttle=delta_t)
-        self.commanded_state.altitude = 0
-        self.commanded_state.Va = 0
-        self.commanded_state.phi = 0
-        self.commanded_state.theta = 0
-        self.commanded_state.chi = 0
+        self.commanded_state.altitude = h_c
+        self.commanded_state.Va = cmd.airspeed_command
+        self.commanded_state.phi = phi_c
+        self.commanded_state.theta = theta_c
+        self.commanded_state.chi = chi_c
         return delta, self.commanded_state
 
     def saturate(self, input, low_limit, up_limit):
