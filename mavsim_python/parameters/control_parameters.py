d042df593b0449e5d4192be79b4ddcc6bfc13fb2
sokuda27 <sonampmokuda@gmail.com>
2025-04-03 16:20:12 -0400

    edited params

--- mavsim_python/parameters/control_parameters.py
+++ mavsim_python/parameters/control_parameters.py
@@ -15,7 +15,7 @@ sigma = 0.005  # low pass filter gain for derivative
 
 #----------roll loop-------------
 # get transfer function data for delta_a to phi
-wn_roll = 22 # error in roll when aileron saturates
+wn_roll = 10 # error in roll when aileron saturates
 zeta_roll = 0.707 # damping ratio for roll attitude loop
 roll_kp = wn_roll**2/TF.a_phi2
 roll_kd = (2.0 * zeta_roll * wn_roll - TF.a_phi1) / TF.a_phi2
@@ -29,11 +29,11 @@ course_kp = 2 * zeta_course * wn_course * Va0 / gravity
 course_ki = wn_course**2 * Va0 / gravity
 
 #----------yaw damper-------------
-yaw_damper_p_wo = 0.4 # washout filter cutoff frequency
+yaw_damper_p_wo = 0.45 # washout filter cutoff frequency
 yaw_damper_kr = 0.2 # gain for yaw damper
 
 #----------pitch loop-------------
-wn_pitch = 24.0 # nat freq for pitch
+wn_pitch = 15.0 # nat freq for pitch
 zeta_pitch = 0.707 # damping ratio for pitch attitude loop
 pitch_kp = (wn_pitch**2 - TF.a_theta2) / TF.a_theta3
 pitch_kd = (2.0 * zeta_pitch * wn_pitch - TF.a_theta1) / TF.a_theta3
@@ -50,9 +50,14 @@ altitude_ki = wn_altitude**2 / K_theta_DC / Va0
 altitude_zone = 10.0
 
 #---------airspeed hold using throttle---------------
-wn_airspeed_throttle = 3.0 # nat freq airspeed hold loop
+wn_airspeed_throttle = 1.5 # nat freq airspeed hold loop
 zeta_airspeed_throttle = 2 # damping ratio for throttle loop
 airspeed_throttle_kp = (2.0 * zeta_airspeed_throttle * wn_airspeed_throttle - TF.a_V1) / TF.a_V2
 airspeed_throttle_ki = wn_airspeed_throttle**2 / TF.a_V2
 # airspeed_throttle_kp = 0
 # airspeed_throttle_ki = 0
+
+#----------climb rate hold using pitch-------------
+climbrate_kp = 1.0
+climbrate_ki = 0.0
+climbrate_kd = 1.0
